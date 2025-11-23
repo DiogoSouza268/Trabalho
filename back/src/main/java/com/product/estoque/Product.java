375a7093230f78312fb47c714439475653d63605
gustavolimass <gustavokl@hotmail.com>
2025-11-22 21:20:17 -0300

    test: add test unit of controller with mockito

--- back/src/main/java/com/product/estoque/Product.java
+++ back/src/main/java/com/product/estoque/Product.java
@@ -5,8 +5,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
-// ARQUIVO 3/5: A "ENTIDADE" (Produto.java)
-// (Com o seu novo pacote)
 @Entity
 public class Product {
 
@@ -16,23 +14,10 @@ public class Product {
     private String nome;
     private int quantidade;
 
-    // Getters e Setters
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public String getNome() {
-        return nome;
-    }
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-    public int getQuantidade() {
-        return quantidade;
-    }
-    public void setQuantidade(int quantidade) {
-        this.quantidade = quantidade;
-    }
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+    public int getQuantidade() { return quantidade; }
+    public void setQuantidade(int quantidade) { this.quantidade = quantidade; }
 }
